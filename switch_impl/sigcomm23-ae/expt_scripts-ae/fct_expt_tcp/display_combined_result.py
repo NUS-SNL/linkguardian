76e9a4f5c69a68509b102f055bfe77221753b211
Raj Joshi <rajkiranjoshi@gmail.com>
2023-10-06 23:11:51 +0800

    AE: cosmetic FCT display combined

--- switch_impl/sigcomm23-ae/expt_scripts-ae/fct_expt_tcp/display_combined_result.py
+++ switch_impl/sigcomm23-ae/expt_scripts-ae/fct_expt_tcp/display_combined_result.py
@@ -4,19 +4,22 @@ import pandas as pd
 
 base_path = '~/traces/fct_expt_tcp'
 expt_names = ['10-3', '10-3_lg', '10-3_lg_nb']
+readable_expt_names = {'10-3':'No Protection', '10-3_lg':'LinkGuardian', '10-3_lg_nb':'LinkGuardianNB'}
 
 summary_dfs = []
 
 for expt_name in expt_names:
     pkl_file = base_path + '/' + expt_name + '/24387B_fct_combined-summary.pkl'
     summary_df = pd.read_pickle(pkl_file)
-    summary_df = summary_df.rename(columns={'fct1': expt_name})
+    col_name = readable_expt_names[expt_name]
+    summary_df = summary_df.rename(columns={'fct1': col_name})
     summary_dfs.append(summary_df)
 
 concat_list = []
 
 for i in range(len(summary_dfs)):
-    concat_list.append(summary_dfs[i][[expt_names[i]]])
+    col_name = readable_expt_names[expt_names[i]]
+    concat_list.append(summary_dfs[i][[col_name]])
 
 combined_result = pd.concat(concat_list, axis=1)
 
