fe7721ce196498ee05096f92b3b62300d9436bc8
Raj Joshi <rajkiranjoshi@gmail.com>
2023-10-10 17:11:12 +0800

    switch AE: better display of throughput results

--- switch_impl/sigcomm23-ae/expt_scripts-ae/throughput_expt/process_all_expts.sh
+++ switch_impl/sigcomm23-ae/expt_scripts-ae/throughput_expt/process_all_expts.sh
@@ -7,7 +7,12 @@ source $mydir/../common/bash_common.sh
 json_parsing_script=$mydir/../../../data_analysis/throughput_expt/parse_iperf3_json.py
 throughput_processing_script=$mydir/../../../data_analysis/throughput_expt/get_avg_throughput.py
 
-printf "%10s\t%s\n" "expt_name" "avg_throughput(Gbps)"
+declare -A expt_display_name
+expt_display_name["10-3"]="No_Protection"
+expt_display_name["10-3_lg"]="LinkGuardian"
+expt_display_name["10-3_lg_nb"]="LinkGuardianNB"
+
+printf "%15s\t\t%s\n" "expt_name" "avg_throughput(Gbps)"
 
 for expt_name in 10-3 10-3_lg 10-3_lg_nb; do
     # first parse the json file
@@ -15,7 +20,9 @@ for expt_name in 10-3 10-3_lg 10-3_lg_nb; do
     # then process the data file
     avg_throughput=`conda run -n lg-data-analysis python3 ../../data_analysis/throughput_expt/get_avg_throughput.py ~/traces/throughput_expt/$expt_name-cubic.dat 5 | grep Avg | cut -d':' -f2`
     
-    printf "%10s\t%s\n" $expt_name $avg_throughput 
+    printf "%15s\t\t%s\n" ${expt_display_name["$expt_name"]} $avg_throughput 
+
+    # printf "%10s\t%s\n" $expt_name $avg_throughput 
 done
 
 
